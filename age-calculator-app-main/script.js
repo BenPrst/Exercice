31e629b5c893cdca6f0f309a076856d2c46906e1
Benjamin Porsont <porsont.benjamin@gmail.com>
2023-05-05 16:00:14 +0200

    modif

--- age-calculator-app-main/script.js
+++ age-calculator-app-main/script.js
@@ -3,7 +3,7 @@ let button = document.getElementById("submit");
 //Current Date
 
 let date = new Date();
-let day = date.getDay();
+let day = date.getDate();
 let month = date.getMonth();
 let year = date.getFullYear();
 
@@ -14,13 +14,38 @@ button.addEventListener("click", () => {
   let monthOfBirth = document.getElementById("month").value;
   let yearOfBirth = document.getElementById("year").value;
 
-  //Verify if the fields are all complete
+  //Verify if the fields are completed
 
   if (
     dayOfBirth.length === 0 ||
     monthOfBirth.length === 0 ||
     yearOfBirth.length === 0
   ) {
-    console.log("empty");
+    return console.log("empty");
+  } else if (dayOfBirth < 1 || dayOfBirth > 31) {
+    return console.log("not a valid day");
+  } else if (monthOfBirth < 1 || monthOfBirth > 12) {
+    return console.log("not a valid month");
+  } else if (yearOfBirth > year) {
+    return console.log("not a valid year");
+  } else {
+    let d;
+    let m;
+    let y = year - yearOfBirth;
+    if (monthOfBirth > month) {
+      m = 12 + month - monthOfBirth;
+      y--;
+    } else {
+      m = month - monthOfBirth;
+    }
+    if (dayOfBirth > day) {
+      d = 30 + day - dayOfBirth;
+      m--;
+    } else {
+      d = day - dayOfBirth;
+    }
+    document.getElementById("y").textContent = y;
+    document.getElementById("m").textContent = m;
+    document.getElementById("d").textContent = d;
   }
 });
